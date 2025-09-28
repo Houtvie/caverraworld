63bf8947deb78ebb7c1b33e6511c62ec3c8c6f66
Houtvie <asesprofuto@gmail.com>
2025-09-28 02:53:07 +0200

    push

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  content: ["./*.html", "./js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#4B0082',
+        'accent-gold': '#FFD700',
+        'accent-emerald': '#008000',
+        'accent-red': '#FF0000',
+        'dark-bg': '#1a1a1a'
+      },
+      fontFamily: {
+        'sans': ['Montserrat', 'sans-serif'],
+        'heading': ['Poppins', 'sans-serif'],
+        'italic': ['Playfair Display', 'serif']
+      }
+    }
+  },
+  plugins: [],
+}
